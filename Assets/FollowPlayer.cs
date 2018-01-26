c84910466eb2dfc31b322e71967e842be42a39d7
Mário Gil <mg_mesquita@hotmail.com>
2018-01-26 21:45:18 +0000

    Building Prefab and character movement

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = player.transform.position;
+	}
+}
