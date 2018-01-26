213dbc67cebafc2b0a5e0d4b5339c95fccb5c40a
Mário Gil <mg_mesquita@hotmail.com>
2018-01-26 19:32:10 +0000

    Player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity;
+	public float jumpForce;
+
+	private Rigidbody2D rigidBody;
+
+	void Start() {
+		rigidBody = this.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+
+		if (Input.GetButtonDown ("Up")) {
+			rigidBody.AddForce (Vector2.up * jumpForce);
+		}
+
+		if (Input.GetButton ("Right")) {
+			this.transform.Translate (Vector3.right * velocity);
+		} else if (Input.GetButton ("Left")) {
+			this.transform.Translate (Vector3.left * velocity);
+		}
+	}
+}
