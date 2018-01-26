7125730fe4518a23302e99d2ea0c36ddcd657a82
Mário Gil <mg_mesquita@hotmail.com>
2018-01-26 19:02:41 +0000

    a

---
+++ Assets/Scripts/AutistKid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutistKid : MonoBehaviour {
+
+	private bool helmetOn = true;
+	private AudioSource audioSource;
+	private Rigidbody2D rigidBody;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadGame() {
+		Application.LoadLevel ("Game");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
