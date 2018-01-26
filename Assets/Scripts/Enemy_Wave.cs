e4d02e2d1bf7cd767635fdbe5113ac347509dd6b
Unknown <guilherme631999@gmail.com>
2018-01-26 21:57:06 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Enemy_Wave.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Wave : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Sound_Wave")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
