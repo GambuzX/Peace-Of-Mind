f16bd0d6aefbc089b8c993f44c7cfe9bf7bc2a55
Mário Gil <mg_mesquita@hotmail.com>
2018-01-27 15:06:48 +0000

    Street

---
+++ Assets/Scripts/Player/Helmet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helmet : MonoBehaviour {
+
+	public bool helmetOff = false;
+
+	private Health health;
+    private SoundWaves[] soundWaves;
+
+	// Use this for initialization
+	void Start () {
+		health = GetComponent<Health> ();
+        soundWaves = GameObject.FindObjectsOfType<SoundWaves>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Helmet")) {
+			helmetOff = true;
+			health.ReduceHealth ();
+			Debug.Log("Helmet taken off");
+		} else {
+			helmetOff = false;
+			health.RecoverHealth ();
+		}
+
+        ControlWaves();
+	}
+
+    void ControlWaves()
+    {
+        foreach (SoundWaves waves in soundWaves)
+        {
+            waves.GetComponent<Animator>().SetBool("HelmetOff", helmetOff);
+        }
+
+        /*if (helmetOff)
+        {
+            foreach (SoundWaves waveParent in soundWaves)
+            {
+                waveParent.GetComponent<Animator>().enabled = true;
+            }
+        }
+        else
+        {
+            foreach (SoundWaves waveParent in soundWaves)
+            {
+                waveParent.GetComponent<Animator>().enabled = false;
+            }
+        }*/
+    }
+}
