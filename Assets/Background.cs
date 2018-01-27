9e8bcc30cb8a5b557f51b47759f05b4e7cd8413d
Mário Gil <mg_mesquita@hotmail.com>
2018-01-27 01:13:17 +0000

    Buildings, static and crates

---
+++ Assets/Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public Camera gameCamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(gameCamera.transform.position.x, gameCamera.transform.position.y, gameCamera.transform.position.z + 10);
+	}
+}
