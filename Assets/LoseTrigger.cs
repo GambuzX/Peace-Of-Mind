993986a92768682ffb2ed8d5bd66e3cfca51cb3c
Mário Gil <mg_mesquita@hotmail.com>
2018-01-28 02:17:55 +0000

    Much stuff

---
+++ Assets/LoseTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Application.LoadLevel("LoseScreen");
+        }
+    }
+}
