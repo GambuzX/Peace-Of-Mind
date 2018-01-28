1645375c548d046b4951787401f5600778f3b540
Mário Gil <mg_mesquita@hotmail.com>
2018-01-28 12:31:01 +0000

    Instructions

---
+++ Assets/StopRunning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRunning : MonoBehaviour {
+
+    public bool stopRunning = false;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        animator.SetBool("stopRunning", stopRunning);
+
+        if (stopRunning)
+        {
+            animator.SetLayerWeight(1, 0f);
+        }
+    }
+}
