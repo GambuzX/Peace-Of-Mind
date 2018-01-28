f30deec8c36d86ced3405eea9b91713575259a97
Mário Gil <mg_mesquita@hotmail.com>
2018-01-28 15:17:05 +0000

    Final touches

---
+++ Assets/WinController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinController : MonoBehaviour {
+
+    public bool winGame = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (winGame)
+        {
+            Application.LoadLevel("WinScreen");
+        }
+
+	}
+}
