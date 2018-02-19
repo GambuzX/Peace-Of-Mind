3187b7840ecd3f8da83b753f80770312f17517ae
Mário Gil <mg_mesquita@hotmail.com>
2018-02-19 22:31:48 +0000

    Pause system working

---
+++ Assets/Pause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject PauseScreen;
+
+    private bool isPaused = false;
+    private float initialFixedDeltaTime;
+    private float lastPlayerHealth;
+
+    private Health health;
+    private CameraShake gameCamera;
+
+    // Use this for initialization
+    void Start () {
+        initialFixedDeltaTime = Time.fixedDeltaTime;
+        health = GameObject.FindObjectOfType<Health>();
+        gameCamera = GameObject.FindObjectOfType<CameraShake>();
+        lastPlayerHealth = health.playerHealth;
+        PauseScreen.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Pause") && !isPaused)
+        {
+            isPaused = true;
+            PauseScreen.SetActive(true);
+            gameCamera.enabled = false;
+            Time.timeScale = 0;
+            Time.fixedDeltaTime = 0;
+
+            lastPlayerHealth = health.playerHealth;
+        }
+        else if (Input.GetButtonDown("Pause") && isPaused)
+        {
+            isPaused = false;
+            PauseScreen.SetActive(false);
+            gameCamera.enabled = true;
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = initialFixedDeltaTime;
+        }
+
+        if(isPaused)
+        {
+            health.playerHealth = lastPlayerHealth;
+        }
+    }
+}
